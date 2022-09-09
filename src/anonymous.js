342d1246485cf86acfcc4f4f020b01d80eadb1f1
MICHAEL2JOSEPH <monmike11@gmail.com>
2022-09-09 10:19:45 +0100

    third push

---
+++ src/anonymous.js
@@ -0,0 +1,23 @@
+// import { getAuth, signInAnonymously, linkWithCredential, EmailAuthProvider } from "firebase/auth";
+
+// const auth = getAuth();
+// signInAnonymously(auth)
+// linkWithCredential(auth.currentUser, credential)
+//     .then((usercred) => {
+//         const user = usercred.user;
+//         console.log("Anonymous account successfully upgraded", user);
+//     }).catch((error) => {
+//         console.log("Error upgrading anonymous account", error);
+//     })
+//     .then(() => {
+//         // Signed in..
+//     })
+//     .catch((error) => {
+//         const errorCode = error.code;
+//         const errorMessage = error.message;
+//         // ...
+//     });
+
+
+
+// const credential = EmailAuthProvider.credential(email, password);
---
+++ src/db.js
@@ -0,0 +1,76 @@
+import firebase from "firebase/auth";
+import "firebase/database";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const config = {
+    apiKey: "AIzaSyB_04DyzQdimNkRaxlTI-nr07B3olLnNtI",
+    authDomain: "chatvue-43cab.firebaseapp.com",
+    databaseURL: "https://chatvue-43cab-default-rtdb.firebaseio.com",
+    projectId: "chatvue-43cab",
+    storageBucket: "chatvue-43cab.appspot.com",
+    messagingSenderId: "861968020629",
+    appId: "1:861968020629:web:848ca9a2b531da1386179c",
+    measurementId: "G-80N8G5R7K0"
+}
+
+const db = firebase.initializeAuth(config);
+export default db;
+
+firebase.initializeApp(config);
+
+// Create a Recaptcha verifier instance globally
+// Calls submitPhoneNumberAuth() when the captcha is verified
+window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier(
+    "recaptcha-container", {
+        size: "normal",
+        callback: function(response) {
+            submitPhoneNumberAuth();
+        }
+    }
+);
+
+// This function runs when the 'sign-in-button' is clicked
+// Takes the value from the 'phoneNumber' input and sends SMS to that phone number
+function submitPhoneNumberAuth() {
+    var phoneNumber = document.getElementById("phoneNumber").value;
+    var appVerifier = window.recaptchaVerifier;
+    firebase
+        .auth()
+        .signInWithPhoneNumber(phoneNumber, appVerifier)
+        .then(function(confirmationResult) {
+            window.confirmationResult = confirmationResult;
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+}
+
+// This function runs when the 'confirm-code' button is clicked
+// Takes the value from the 'code' input and submits the code to verify the phone number
+// Return a user object if the authentication was successful, and auth is complete
+// function submitPhoneNumberAuthCode() {
+//     var code = document.getElementById("code").value;
+//     confirmationResult
+//         .confirm(code)
+//         .then(function(result) {
+//             var user = result.user;
+//             console.log(user);
+//         })
+//         .catch(function(error) {
+//             console.log(error);
+//         });
+// }
+
+//This function runs everytime the auth state changes. Use to verify if the user is logged in
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        console.log("USER LOGGED IN");
+    } else {
+        // No user is signed in.
+        console.log("USER NOT LOGGED IN");
+    }
+});
+
+const app = initializeApp(config);
+const analytics = getAnalytics(app);
